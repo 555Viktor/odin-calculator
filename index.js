8d08e84a3607006525646e6564b95ad2d17ac35a
555Viktor <victor.vaskov2002@gmail.com>
2024-10-23 14:22:16 +0300

    Add initial display functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Display DOM els
 const calcDisplayContainer = document.querySelector('.calc-display-wrapper');
 const calcDisplay = document.querySelector('.calc-display');
+calcDisplay.textContent = 0; //Default display value
 
 // Node-lists for all button types
 const calcAllBtns = document.querySelectorAll('.btn-calc');
@@ -12,3 +13,22 @@ const btnDel = document.querySelector('.btn-del');
 const btnClear = document.querySelector('.btn-clear');
 const btnEquals = document.querySelector('.btn-equals');
 
+// Append to display functions
+
+function appendToDisplay (n) {
+    calcDisplay.textContent = n;
+}
+
+function clearDisplay () {
+    calcDisplay.textContent = 0;
+}
+
+calcNumBtns.forEach(btnNum => {
+    btnNum.addEventListener('click', () => {
+        appendToDisplay(btnNum.textContent);
+    });
+});
+
+btnClear.addEventListener('click', () => {
+   clearDisplay() 
+})
