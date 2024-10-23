f8956c9efc07e096a0e4991c7903f75d0de9c396
555Viktor <victor.vaskov2002@gmail.com>
2024-10-23 14:06:21 +0300

    Add and style footer, improve colors, add JS DOM els

---
+++ index.js
@@ -0,0 +1,14 @@
+// Display DOM els
+const calcDisplayContainer = document.querySelector('.calc-display-wrapper');
+const calcDisplay = document.querySelector('.calc-display');
+
+// Node-lists for all button types
+const calcAllBtns = document.querySelectorAll('.btn-calc');
+const calcNumBtns = document.querySelectorAll('.btn-num');
+const calcOperationBtns = document.querySelectorAll('.btn-operation');
+
+// DOM calc operation buttons
+const btnDel = document.querySelector('.btn-del');
+const btnClear = document.querySelector('.btn-clear');
+const btnEquals = document.querySelector('.btn-equals');
+
