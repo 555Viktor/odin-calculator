0eaf9ac03f2107e5afee809d273abf242291a452
555Viktor <victor.vaskov2002@gmail.com>
2024-10-28 15:25:26 +0200

    Add DOM variables

---
+++ index.js
@@ -0,0 +1,12 @@
+// DOM elements
+const calcDisplayContainer = document.querySelector('.calc-display-wrapper')
+const calcDisplay = document.querySelector('.calc-display');
+
+const numBtns = document.querySelectorAll('.btn-num');
+const operationBtns = document.querySelectorAll('.btn-operation');
+
+const btnDecimal = document.querySelector('.btn-decimal');
+const btnEquals = document.querySelector('.btn-equals');
+const btnDelete = document.querySelector('.btn-del');
+const btnClear = document.querySelector('.btn-clear');
+
