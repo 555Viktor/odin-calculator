2bfc6f85fbd4cf8ee27d26c2042ba76a1c4e293b
555Viktor <victor.vaskov2002@gmail.com>
2024-10-28 16:18:54 +0200

    Add basic diplay and num btn functionality

--- index.js
+++ index.js
@@ -10,3 +10,55 @@ const btnEquals = document.querySelector('.btn-equals');
 const btnDelete = document.querySelector('.btn-del');
 const btnClear = document.querySelector('.btn-clear');
 
+let currentNum = '';
+let previousNum = '';
+let operator = '';
+let result = '';
+
+let isOperatorUsed = false;
+
+function calculate (currNum, prevNum, op) {
+    let curr = parseFloat(currNum);
+    let prev = parseFloat(prevNum);
+
+    if (isNaN(currNum) || isNaN(prevNum)){
+        return 'Error';
+    }
+
+    switch (op) {
+        case '+':
+            return curr + prev;
+
+        case '-':
+            return curr - prev;
+            
+        case '*':
+            return curr * prev;
+
+        case '/':
+            if (prevNum == '0') return 'Error'
+            return curr / prev;
+
+        default:
+            return 'Error'
+    }
+
+}
+
+function updateDisplay (num) {
+    calcDisplay.textContent = num;
+}
+
+numBtns.forEach(numBtn => {
+    numBtn.addEventListener('click', () => {
+        if (isOperatorUsed) {
+            currentNum = numBtn.textContent;
+            isOperatorUsed = false;
+        } else {
+            currentNum += numBtn.textContent;
+        }
+
+        updateDisplay(currentNum);
+    })
+})
+
